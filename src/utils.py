1323b946179a36303d3e2f035d8fdb9032bc2842
AlexProsh <pravnet@mail.ru>
2023-12-01 08:22:59 +0100

    Add dvc (data version control)

---
+++ src/utils.py
@@ -0,0 +1,8 @@
+import os
+
+for (root, dirs, files) in os.walk('../data/raw/val', topdown=True):
+    for file in files:
+        if file[file.find('-')+1:file.find('-')+3] != '09':
+
+            file_path = os.path.join(root,file)
+            os.remove(file_path)
